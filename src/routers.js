bbfe723377dbd926b42ef374766a9b1d1ee9e821
ouhouh1936 <ouhouh1936@gamil.com>
2020-11-29 18:42:39 +0900

    ssss

---
+++ src/routers.js
@@ -0,0 +1,20 @@
+//global router
+const HOME = `/`;
+const CONTACT = `/contact`;
+
+//development router
+const DEVELOPMENT = `/devlopment`;
+const NODEJS = `/nodejs`;
+const MONGODB = `mongoDb`;
+const JAVASCRIPT = `/javaScript`;
+const REACT = `/react`;
+
+export const routers = {
+  HOME,
+  CONTACT,
+  DEVELOPMENT,
+  NODEJS,
+  MONGODB,
+  JAVASCRIPT,
+  REACT,
+};
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import morgan from "morgan";
+import globalRouter from "./router/globalRouter";
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.set("view engine", "pug");
+app.use(morgan(`dev`));
+
+app.use("/", globalRouter);
+
+app.listen(PORT, () => {
+  console.log(`✅ ${PORT} server start`);
+});
